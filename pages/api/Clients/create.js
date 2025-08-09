9b4d9edd701354091037908ffd19b428179c3f3d
Samuel <samuelabreudev@gmail.com>
2025-08-09 02:33:10 -0300

    REST-cli

---
+++ pages/api/Clients/create.js
@@ -0,0 +1,15 @@
+/**
+ * Nome do arquivo: create.js
+ * Data de criação: 09/08/2025
+ * Autor: Samuel Abreu
+ * Matrícula: 01762413
+ *
+ * Descrição:
+ * Este arquivo vai ser responsável pelas funcionalidades de criação de clientes no banco de dados.
+ *
+ * Este script é parte o curso de ADS.
+ */
+
+export default function createClient(req, res){
+    
+}
