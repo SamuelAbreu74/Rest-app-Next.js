d266b559768d062d0a5a6624012b0150c000a29a
Samuel <samuelabreudev@gmail.com>
2025-08-25 01:15:06 -0300

    ADS: 01762413 (DB/CRUD CLIENTES)

---
+++ pages/api/Clients/delete/[id].js
@@ -0,0 +1,29 @@
+/**
+ * Nome do arquivo: delete.js
+ * Data de criação: 24/08/2025
+ * Autor: Samuel de Abreu Moisés
+ * Matrícula: 01762413
+ *
+ * Descrição:
+ * Este arquivo vai ser responsável pela rota que implementa as funcionalidade de apagar/deletar um client com requisições HTTP.
+ *
+ * Este script é parte o curso de ADS.
+ */
+import { deleteClient } from "@/repository/client.js"
+
+export default async function handler(req, res){
+    if(req.method !== 'DELETE'){
+        res.setHeader('Allow', ['DELETE']);
+        res.status(405).end(`Method ${req.method} not Allowed!`)
+    }
+
+    try {
+        const {id} = req.query
+        await deleteClient(id)
+        res.status(200).json({message: "Client deleted successfully!"});
+        
+    } catch (error) {
+        console.log("Failed to deleted the client", error);
+        res.status(500).json({message: "An error occureed on the server"});
+    }
+}
---
+++ pages/api/Clients/read.js
@@ -0,0 +1,35 @@
+/**
+ * Nome do arquivo: read.js
+ * Data de criação: 09/08/2025
+ * Autor: Samuel de Abreu Moisés
+ * Matrícula: 01762413
+ *
+ * Descrição:
+ * Este arquivo vai ser responsável pela rota que implementa as funcionalidade de obter/ler os dados dos clientes com requisições HTTP.  
+ *
+ * Este script é parte o curso de ADS.
+ */
+
+
+import { getClient } from "@/repository/client";
+
+
+export default async function handler(req, res) {
+    if (req.method !== 'GET'){
+      res.setHeader('Allow', ['GET']);
+      res.status(405).end(`Method ${req.method} not Allowed!`);
+    }
+
+    try {
+        const clients = await getClient();
+
+        res.status(200)
+        res.send(clients)
+    } catch (error) {
+      console.log("Failure on clients search", error);
+      res.status(500).json({message: "An error occurred on the server"});
+    }
+    
+}
+
+
