d5255e8c9a27f192097f391d725630691bf73843
Samuel <samuelabreudev@gmail.com>
2025-08-25 01:36:33 -0300

    ADS: 01762413 (DB/CRUD CLIENTES)

--- pages/api/Clients/create.js
+++ pages/api/Clients/create.js
@@ -1,15 +1,35 @@
 /**
  * Nome do arquivo: create.js
- * Data de criação: 09/08/2025
- * Autor: Samuel Abreu
+ * Data de criação: 14/08/2025
+ * Autor: Samuel de Abreu Moisés
  * Matrícula: 01762413
  *
  * Descrição:
- * Este arquivo vai ser responsável pelas funcionalidades de criação de clientes no banco de dados.
+ * Este arquivo vai ser responsável pela rota que implementa as funcionalidades de criação de clientes com requisições HTTP.
  *
  * Este script é parte o curso de ADS.
  */
 
-export default function createClient(req, res){
-    
+import { createClient } from "@/repository/client"
+
+export default async function handler(req, res){
+    if (req.method !== 'POST'){
+        res.setHeader('Allow', ['POST'])
+        res.status(405).end(`Method ${req.method} not Allowed!`)    
+    }
+
+    try {
+        const clientData = req.body;
+
+        if(!clientData.name || !clientData.email || !clientData.phone || !clientData.address){
+            res.status(400).json({message: "It is necessary to fill in all fields of the form!"})
+        }
+        const newClient = await createClient(clientData);
+        res.status(201).json({id: newClient.id, message: "New client created successfully!"});
+    } catch (error) {
+        console.log("Failed to create a new client", error);
+        res.status(500).json({message: "An error occurred on the server"});
+    }
 }
+
+
--- pages/api/Clients/update.js
+++ pages/api/Clients/update.js
@@ -1,15 +1,38 @@
 /**
- * Nome do arquivo: create.js
- * Data de criação: 09/08/2025
- * Autor: Samuel Abreu
+ * Nome do arquivo: update.js
+ * Data de criação: 17/08/2025
+ * Autor: Samuel de Abreu Moisés
  * Matrícula: 01762413
  *
  * Descrição:
- * Este arquivo vai ser responsável pelas funcionalidades de edição/atualização de Clientes no banco de dados.  
+ * Este arquivo vai ser responsável pela rota que implementa as funcionalidades de edição/atualização de Clientes com requisições HTTP.  
  *
  * Este script é parte o curso de ADS.
  */
 
-export default function updateClient(req,res){
+import { updateClient } from "@/repository/client";
+
+
+export default async function handler(req,res){
+    
+    if(req.method !== 'PUT'){
+        res.setHeader('Allow', ['PUT']);
+        res.status(405).end(`Method ${req.method} not Allowed!`);
+    }
+
+    try {
+        const clientToEditData = req.body
+        
+        if(!clientToEditData.id){
+            res.status(400).end('Bad Request: This client don`t exist');
+        }
+        updateClient(clientToEditData)
+        res.status(200).end('client update Successfully!')
+    } catch (error) {
+        console.log("Failure on client update", error);
+        res.status(500).json({message: "An error occurred on the server"});
+    }
+
+
 
 }
