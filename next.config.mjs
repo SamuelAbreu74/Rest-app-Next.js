1cbf76482ad17b0fa05c2036231be956a6b4e1c8
Samuel <samuelabreudev@gmail.com>
2025-09-17 16:01:12 -0300

    ADS: 01762413 (Deploy-js)

--- next.config.mjs
+++ next.config.mjs
@@ -3,39 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   
   output: 'standalone',
-  
-
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { 
-            key: 'Access-Control-Allow-Origin', 
-            value: '*' 
-          },
-          { 
-            key: 'Access-Control-Allow-Methods', 
-            value: 'GET, POST, PUT, DELETE, OPTIONS' 
-          },
-          { 
-            key: 'Access-Control-Allow-Headers', 
-            value: 'Content-Type, Authorization, X-Requested-With' 
-          },
-        ],
-      },
-    ];
-  },
-
-
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/api/:path*',
-      },
-    ];
-  },
 
 };
 
